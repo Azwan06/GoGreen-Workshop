a7d5341c7db4523f37e38986a1e2d35243236079
Azwan <fahrulazwan89@gmail.com>
2026-06-14 13:00:33 +0800

    branch baru

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "uten_gogreen");
+$conn = mysqli_connect("localhost", "root", "", "gogreen");
 
 if (!$conn) {
     die("Connection failed ");
