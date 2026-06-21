1fa5cf87a616567bc9b377d5299f14b6e2568aeb
Hazeeq <raedhazeeq@gmail.com>
2026-06-22 02:35:48 +0800

    test11

--- Public/login.php
+++ Public/login.php
@@ -31,7 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $_SESSION['user_id']  = $user['id'];
                 $_SESSION['fullname'] = $user['fullname'];
                 $_SESSION['email']    = $user['email'];
-                //$_SESSION['role']     = $user['role'];
+                $_SESSION['role']     = $user['role'];
 
                 // Redirect berdasarkan domain email
                 if (str_ends_with($email_lower, '@utem.edu.my')) {
